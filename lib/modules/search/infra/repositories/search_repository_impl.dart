b2403e7118f398c39354027500b5089a9efa69a4
Vinicio  Costa Santos <vinicio.csantos@outlook.com>
2023-01-14 19:32:36 -0300

    implementing repository

--- lib/modules/search/infra/repositories/search_repository_impl.dart
+++ lib/modules/search/infra/repositories/search_repository_impl.dart
@@ -1,10 +1,15 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture_example/modules/search/infra/datasource/search_datasource.dart';
 
 import '../../domain/entities/result_search.dart';
 import '../../domain/errors/errors.dart';
 import '../../domain/repositories/search_repository.dart';
 
 class SearchRepositoryImpl implements SearchRepository {
+
+    final SearchDatasource? datasource;
+
+    SearchRepositoryImpl(this.datasource);
 
   @override
   Future<Either<FailureSearch, List<ResultSearch>>> search(String? searchText) {
--- test/modules/search/infra/repositories/search_repository_impl_test.dart
+++ test/modules/search/infra/repositories/search_repository_impl_test.dart
@@ -1,7 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture_example/modules/search/domain/entities/result_search.dart';
+import 'package:flutter_clean_architecture_example/modules/search/infra/datasource/search_datasource.dart';
+import 'package:flutter_clean_architecture_example/modules/search/infra/models/result_search_model.dart';
+import 'package:flutter_clean_architecture_example/modules/search/infra/repositories/search_repository_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class SearchDatasourceMock extends Mock implements SearchDatasource{}
+
 
 main(){
-  test('', (){
+  final datasource = SearchDatasourceMock();  
 
+  final repository = SearchRepositoryImpl(datasource);
+
+  test('deve retornar uma lista de Resultsearch', () async{
+    when(() => datasource.getSearch(any()))
+    .thenAnswer((_) async => <ResultSearchModel>[]);
+
+    final result = await repository.search('searchText');
+
+    expect(result.fold(id, id), isA<List<ResultSearch>>());
   });
 }
