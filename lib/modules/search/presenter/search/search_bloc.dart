113149f375c7c6b93b223ddde92c4d401e96d76b
Vinicio  Costa Santos <vinicio.csantos@outlook.com>
2023-01-25 00:12:21 -0300

    add debounde time and removing comments

--- lib/modules/search/presenter/search/search_bloc.dart
+++ lib/modules/search/presenter/search/search_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_clean_architecture_example/modules/search/domain/usecases/search_by_text.dart';
 import 'package:flutter_clean_architecture_example/modules/search/presenter/search/states/state.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SearchBloc extends Bloc<String, SearchState> {
   
@@ -10,23 +11,11 @@ class SearchBloc extends Bloc<String, SearchState> {
     on<String>((event, emit) async {
       emit(SearchLoading());
       final result = await usecase(event);
-      emit(result.fold((l) => SearchError(l), (r) => SearchSuccess(r)));
-    });
+      emit(result.fold((l) => SearchError(l), (r) => SearchSuccess(r)));      
+    },
+    transformer: ((events, mapper) {
+        return events.debounceTime(const Duration(milliseconds: 300)).asyncExpand(mapper);
+    })
+    );
   }
-
-  // //state changes
-  // //@override
-  // on<E extends String>(EventHandler<E, SearchState> handler, {EventTransformer<E>? transformer}) async {
-  //   // TODO: implement on
-  //   emit(SearchLoading());
-  //   final result = await usecase(handler as String);
-  //   emit(result.fold((l) => SearchError(l), (r) => SearchSuccess(r)));
-  // }
-
-  // Stream<SearchState> mapEventToState(String searchText) async* {
-  //   yield SearchLoading();
-  //   final result = await usecase(searchText);
-  //   //await Future.delayed(Duration(seconds: 2));
-  //   yield result.fold((l) => SearchError(l), (r) => SearchSuccess(r));
-  // }
 }
